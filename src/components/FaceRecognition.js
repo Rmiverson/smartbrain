a599708b2c7f4a295a72962c045f4197eadec8da
rmiverson <rmiverson99@gmail.com>
2023-02-14 16:05:45 -0800

    added a bunch of new components, got rid of default react stuff, added sass.

---
+++ src/components/FaceRecognition.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const FaceRecognition = () => {
+    return (
+        <div className='face-recognition'>
+
+        </div>
+    )
+}
+
+export default FaceRecognition
---
+++ src/components/ImageLinkForm.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const ImageLinkForm = () => {
+    return (
+        <form className='image-link-form'>
+
+        </form>
+    )
+}
+
+export default ImageLinkForm
