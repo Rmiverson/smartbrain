c7a47304cffd26c2130b2ec81ed5e891e1f1e413
rmiverson <rmiverson99@gmail.com>
2023-02-14 16:27:06 -0800

    changed nav item styling to tachyons styling

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,11 +1,11 @@
 import React from 'react'
 
 const Nav = () => {
-    return (
-        <nav className='nav'>
-            <div>Sign Out</div>
-        </nav>
-    )
+  return (
+    <nav className='nav'>
+      <div className='f3 link dim black underline pa3 pointer'>Sign Out</div>
+    </nav>
+  )
 }
 
 export default Nav
