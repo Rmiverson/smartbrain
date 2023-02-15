62969db27a4ddbbbee196e9eb07ac9ab0aea5bb5
rmiverson <rmiverson99@gmail.com>
2023-02-14 16:26:12 -0800

    updated indenting

--- src/components/Logo.js
+++ src/components/Logo.js
@@ -1,11 +1,11 @@
 import React from 'react'
 
 const Logo = () => {
-    return (
-        <div className='logo'>
+  return (
+    <div className='logo'>
 
-        </div>
-    )
+    </div>
+  )
 }
 
 export default Logo
